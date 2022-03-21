419a086247161fccb861faafc2048be106253abc
RAVI KR <53385448+ravi-hash@users.noreply.github.com>
2022-03-21 15:46:12 +0530

    Add files via upload

---
+++ extension/resize.js
@@ -0,0 +1,4 @@
+var h = window.innerHeight;
+var w = window.innerWidth;
+document.body.style.backgroundImage = 'url("https://images7.alphacoders.com/736/736462.png/${w}x${h}")'
+ //https://source.unsplash.com/random
